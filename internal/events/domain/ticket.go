f6273d7a84675cdb7d296d3a4e15b3639327656d
Igor Gabriel <44100494+Kiluameta@users.noreply.github.com>
2024-06-24 13:41:46 -0300

    chore: calculate and validate price of ticket

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,6 +1,12 @@
 package domain
 
+import "errors"
+
 type TicketType string
+
+var (
+	ErrTicketPriceGreater = errors.New("ticket price must be greater than zero")
+)
 
 const (
 	TicketTypeFull TicketType = "full"
@@ -13,4 +19,21 @@ type Ticket struct {
 	Spot       *Spot
 	TicketType TicketType
 	Price      float64
+}
+
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeFull || ticketType == TicketTypeHalf
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrTicketPriceGreater
+	}
+	return nil
 }
