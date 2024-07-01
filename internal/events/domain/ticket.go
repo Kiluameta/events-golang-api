0dd12fe8beca571333350192168c5de0a041b225
Igor Gabriel <44100494+Kiluameta@users.noreply.github.com>
2024-07-01 15:44:06 -0300

    chore: create function NewTicket

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,11 +1,16 @@
 package domain
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
 
 type TicketType string
 
 var (
 	ErrTicketPriceGreater = errors.New("ticket price must be greater than zero")
+	ErrInvalidTicketType = errors.New("invalid ticket type")
 )
 
 const (
@@ -19,6 +24,27 @@ type Ticket struct {
 	Spot       *Spot
 	TicketType TicketType
 	Price      float64
+}
+
+func NewTicket(event *Event, spot *Spot, ticketType TicketType) (*Ticket, error) {
+	if !IsValidTicketType(ticketType) {
+		return nil, ErrInvalidTicketType
+	}
+	
+	ticket := &Ticket {
+		ID: uuid.New().String(),
+		EventID: event.ID,
+		Spot: spot,
+		TicketType: ticketType,
+		Price: event.Price,
+	}
+
+	ticket.CalculatePrice()
+
+	if err := ticket.Validate(); err != nil {
+		return nil, err
+	}
+	return ticket, nil
 }
 
 func IsValidTicketType(ticketType TicketType) bool {
